6f2915aac75bf7dd5eb4b7ce4ad6b342e3960918
Jim <blyzzcanada@gmail.com>
2023-09-03 20:39:54 -0700

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-URL='https://discord.com/api/webhooks/1132488373265760266/48oDcTn0Mup1jnZTpOctN0L--qqLOgfEA1_dlIikgvQyi1r5-mS-Jc9_-uF6MqJecvC0'
+URL=''
 
 PINK=16761035
 CRIMSON=14423100
