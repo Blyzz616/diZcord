b9401637c9acd28abfd3a9e968306d457f25a228
Jim <blyzzcanada@gmail.com>
2023-10-14 13:58:22 -0700

    Update discon.sh

--- discon.sh
+++ discon.sh
@@ -28,21 +28,17 @@ READER(){
       fi
 
       # Session Time
-      if [[ $GAMETIME -eq 0 ]];
-      then
+      if [[ $GAMETIME -eq 0 ]]; then
         curl -H "Content-Type: application/json" -X POST -d "{\"embeds\": [{ \"color\": \"$LAVENDER\", \"title\": \"The mods on the server appear to be out of date.\", \"description\": \"Restarting the server to update all mods.\nPlease wait a minute before rejoining.\" }] }" $URL
         /usr/local/bin/pzuser2/restart.sh &
         exit
       fi
 
-      if [[ $GAMETIME -ge 86400 ]];
-      then
+      if [[ $GAMETIME -ge 86400 ]]; then
         UPTIME=$(printf '%dd %dh %dm %ds' $((GAMETIME/86400)) $((GAMETIME%86400/3600)) $((GAMETIME%3600/60)) $((GAMETIME%60)))
-      elif [[ $GAMETIME -ge 3600  ]];
-      then
+      elif [[ $GAMETIME -ge 3600  ]]; then
         UPTIME=$(printf '%dh %dm %ds' $((GAMETIME/3600)) $((GAMETIME%3600/60)) $((GAMETIME%60)))
-      elif [[ $GAMETIME -ge 60 ]];
-      then
+      elif [[ $GAMETIME -ge 60 ]]; then
         UPTIME=$(printf '%dm %ds' $((GAMETIME/60)) $((GAMETIME%60)))
       else
         UPTIME=$(printf '%ds' $((GAMETIME)))
@@ -51,22 +47,18 @@ READER(){
       # Total Time
       TOTAL=$(awk '{ sum += $1 } END { print sum }' /opt/pzserver2/dizcord/playerdb/"$DISCONNSTEAM".total)
 
-      if [[ $TOTAL -ge 86400 ]];
-      then
+      if [[ $TOTAL -ge 86400 ]]; then
         LIFE=$(printf '%dd %dh %dm %ds' $((TOTAL/86400)) $((TOTAL%86400/3600)) $((TOTAL%3600/60)) $((TOTAL%60)))
-      elif [[ $TOTAL -ge 3600  ]];
-      then
+      elif [[ $TOTAL -ge 3600  ]]; then
         LIFE=$(printf '%dh %dm %ds' $((TOTAL/3600)) $((TOTAL%3600/60)) $((TOTAL%60)))
-      elif [[ $TOTAL -ge 60 ]];
-      then
+      elif [[ $TOTAL -ge 60 ]]; then
         LIFE=$(printf '%dm %ds' $((TOTAL/60)) $((TOTAL%60)))
       else
         LIFE=$(printf '%ds' $((GAMETIME)))
       fi
 
      HOURTIME=$(awk '{ sum += $1 } END { print sum }' /opt/pzserver2/dizcord/playerdb/"$DISCONNSTEAM".total)
-     if [[ $HOURTIME -ge 3600  ]];
-      then
+     if [[ $HOURTIME -ge 3600  ]]; then
         HOURS=$(printf '%d Hours' $((HOURTIME/3600)))
      fi
 
