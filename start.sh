a07c7d3e84f3081f083a011686c895397546575a
Jim <blyzzcanada@gmail.com>
2023-11-13 09:52:27 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,19 +3,25 @@
 URL='https://discord.com/api/webhooks/'
 BLUE=45015
 
-MESSAGE="**Rotting Domain** server reanimating now."
+MESSAGE="**Blighted Dominion** server reanimating now."
 
-SRVRINI="rot"
+SRVRINI="blight"
 
-date +%s > /tmp/srvr2-start.time
+date +%s > "/opt/dizcord/times/$SRVRINI-start.time"
 curl -H "Content-Type: application/json" -X POST -d "{\"embeds\": [{ \"color\": \"$BLUE\", \"description\": \"$MESSAGE\" }] }" "$URL"
 
-[[ $(screen -ls | grep -E 'PZ2\s' | wc -l) -eq 0 ]] && screen -dm -s /bin/bash -S PZ2 -Logfile /tmp/PZ2.log && screen -S PZ2 -X stuff "/bin/bash /opt/pzserver2/start-server.sh -servername $SRVRINI ^M"
+[[ $(screen -ls | grep -c -E 'PZ\s') -eq 0 ]] && \
+screen -dm -s /bin/bash -S PZ -L -Logfile /tmp/PZ.log && \
+screen -S PZ -X stuff "/home/pz1/Zomboid/start-server.sh -servername $SRVRINI ^M"
 
-[[ $(screen -ls | grep -E 'PZ2-S-obit' | wc -l) -eq 0 ]] && screen -dm -s /bin/bash -S PZ2-S-obit-Logfile /tmp/PZ2-S-obit.log && screen -S PZ2-S-obit -p 0 -X stuff "/bin/bash /opt/pzserver2/dizcord/obit.sh  ^M"
-[[ $(screen -ls | grep -E 'PZ2-S-discon' | wc -l) -eq 0 ]] && screen -dm -s /bin/bash -S PZ2-S-discon -Logfile /tmp/PZ2-S-discon.log && screen -S PZ2-S-discon -p 0 -X stuff "/bin/bash /opt/pzserver2/dizcord/discon.sh ^M"
-[[ $(screen -ls | grep -E 'PZ2-S-startup' | wc -l) -eq 0 ]] && screen -dm -s /bin/bash -S PZ2-S-startup -Logfile /tmp/PZ2-S-startup.log && screen -S PZ2-S-startup -p 0 -X stuff "/bin/bash /opt/pzserver2/dizcord/startup.sh ^M"
-[[ $(screen -ls | grep -E 'PZ2-S-connect' | wc -l) -eq 0 ]] && screen -dm -s /bin/bash -S PZ2-S-connect -Logfile /tmp/PZ2-S-connect.log && screen -S PZ2-S-connect -p 0 -X stuff "/bin/bash /opt/pzserver2/dizcord/connect.sh ^M"
-[[ $(screen -ls | grep -E 'PZ2-S-chopper' | wc -l) -eq 0 ]] && screen -dm -s /bin/bash -S PZ2-S-chopper -Logfile /tmp/PZ2-S-chopper.log && screen -S PZ2-S-chopper -p 0 -X stuff "/bin/bash /opt/pzserver2/dizcord/chopper.sh ^M"
-[[ $(screen -ls | grep -E 'PZ2-S-shutdown' | wc -l) -eq 0 ]] && screen -dm -s /bin/bash -S PZ2-S-shutdown -Logfile /tmp/PZ2-S-shutdown.log && screen -S PZ2-S-shutdown -p 0 -X stuff "/bin/bash /opt/pzserver2/dizcord/shutdown.sh ^M"
-[[ $(screen -ls | grep -E 'PZ2-S-stream' | wc -l) -eq 0 ]] && screen -dm -s /bin/bash -S PZ2-S-stream -Logfile /tmp/PZ2-S-stream.log && screen -S PZ2-S-stream -p 0 -X stuff "/bin/bash /opt/pzserver2/dizcord/stream.sh ^M"
+[[ $(screen -ls | grep -c -E 'PZ-reader') -eq 0 ]] && \
+screen -dm -s /bin/bash -S PZ-reader -L -Logfile /tmp/PZ-reader.log && \
+screen -S PZ-reader -X stuff "/opt/dizcord/reader.sh ^M"
+
+[[ $(screen -ls | grep -c -E 'PZ-obit') -eq 0 ]] && \
+screen -dm -s /bin/bash -S PZ-obit && \
+screen -S PZ-obit -p 0 -X stuff "/opt/dizcord/obit.sh ^M"
+
+[[ $(screen -ls | grep -c -E 'PZ-bot') -eq 0 ]] && \
+screen -dm -s /bin/bash -S PZ-bot && \
+screen -S PZ-obit -p 0 -X stuff "/usr/bin/node /opt/boidbot/bot.sh ^M"
