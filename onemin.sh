608e5e331d64947e0ba5ed78511931b5113986c6
Jim <blyzzcanada@gmail.com>
2023-12-17 02:49:18 -0800

    Update onemin.sh

--- onemin.sh
+++ onemin.sh
@@ -1,44 +1,44 @@
 #! /bin/bash
 
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 1 minute\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 1 minute\" ^M"
 sleep 5
 # 5 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 1 minute\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 1 minute\" ^M"
 sleep 5
 # 10 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Get. To. Safety!\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Get. To. Safety!\" ^M"
 sleep 5
 # 15 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down soon!\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down soon!\" ^M"
 sleep 5
 # 20 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down soon!\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down soon!\" ^M"
 sleep 5
 # 25 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Get. To. Safety!\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Get. To. Safety!\" ^M"
 sleep 5
 # 30 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 30\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 30\" ^M"
 sleep 5
 # 35 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 25\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 25\" ^M"
 sleep 5
 # 40 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 20\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 20\" ^M"
 sleep 5
 # 45 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Quit messing around. Get to safety NOW!\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Quit messing around. Get to safety NOW!\" ^M"
 sleep 5
 # 50 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 10\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 10\" ^M"
 sleep 5
 # 55 seconds
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 5\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 5\" ^M"
 sleep 1
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 4\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 4\" ^M"
 sleep 1
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 3\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 3\" ^M"
 sleep 1
-screen -S PZ2 -p 0 -X stuff "servermsg \"Server going down in 2\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Server going down in 2\" ^M"
 sleep 1
-screen -S PZ2 -p 0 -X stuff "servermsg \"Back soon!\" ^M"
+screen -S PZ -p 0 -X stuff "servermsg \"Back soon!\" ^M"
