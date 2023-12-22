febd9325e0f2ee591932feab99960ce49f9952e9
Jim <blyzzcanada@gmail.com>
2023-12-21 17:58:25 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,10 +10,15 @@ CURRENT_VERSION=$(/opt/dizcord/current.version)
 if [[ $CURRENT_VERSION !=  $LATEST_VERSION ]]; then
   wget -O "/tmp/$LATEST_VERSION.tar.gz" "https://github.com/Blyzz616/diZcord/archive/$LATEST_VERSION.tar.gz"
   tar -zxvf "/tmp/$LATEST_VERSION.tar.gz" -C /tmp
-  cp -r "/tmp/diZcord-$LATEST_VERSION" /opt/dizcord/
+  sudo mkdir -p /opt/dizcord/
+  sudo chown $(whoami):$(whoami) /opt/dizcord
+  cp -r "/tmp/dizcord-$LATEST_VERSION" /opt/dizcord/
   rm "/tmp/$LATEST_VERSION.tar.gz"
   echo "$LATEST_VERSION" > /opt/dizcord/current.version
 fi
+
+sudo chmod ug+x /opt/dizcord/wizard.sh
+sudo /opt/dizcord/wisard.sh &
 
 #if [[ $(ps aux | grep ProjectZomboid64 | grep -v grep | wc -l ) -eq 1 ]]; then
 #  /opt/dizcord/restart.sh &
