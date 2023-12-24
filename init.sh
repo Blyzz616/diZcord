748df3d35d93c9400c6da87d138189b6f1a81f28
Jim <blyzzcanada@gmail.com>
2023-12-24 02:21:57 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 LATEST_VERSION=$(curl -sL https://api.github.com/repos/Blyzz616/diZcord/releases/latest | jq -r '.tag_name')
 
 # Get current version
-CURRENT_VERSION=$(sudo cat /opt/dizcord/current.version)
+CURRENT_VERSION=$(sudo cat /opt/dizcord/current.version 2>/dev/null)
 
 # get current user name
 I_AM=$(whoami)
@@ -12,12 +12,11 @@ I_AM=$(whoami)
 # Update if necessary
 if [[ "$CURRENT_VERSION" !=  "$LATEST_VERSION" ]]; then
   NO_V=$(echo "$LATEST_VERSION" | sed 's/v//')
-  wget -O "/tmp/dizcord-$NO_V.tar.gz" "https://github.com/Blyzz616/diZcord/archive/$LATEST_VERSION.tar.gz"
-  tar -zxvf "/tmp/dizcord-$NO_V.tar.gz" -C /tmp
   sudo mkdir -p /opt/dizcord/
-  sudo cp -r "/tmp/diZcord-$NO_V/." /opt/dizcord
+  wget -q -O "/tmp/dizcord-$NO_V.tar.gz" "https://github.com/Blyzz616/diZcord/archive/$LATEST_VERSION.tar.gz"
+  tar -zxvf "/tmp/dizcord-$NO_V.tar.gz" -C /opt/dizcord/ --strip-components=1 >/dev/null
   sudo chown -R "$I_AM":"$I_AM" /opt/dizcord
-  rm -r /tmp/dizcord*
+  rm -r /tmp/di?cord*
   echo "$LATEST_VERSION" > /opt/dizcord/current.version
 fi
 
